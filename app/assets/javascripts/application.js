3a7c27aa9cc1a6209a01cd113e87e0256fab29b0
ManuelBradicic42 <manuelbradicic@gmail.com>
2020-11-29 11:23:02 -0800

    Local storage updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,108 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+var map = new Map;
+
+function updateNumberOfOrders3(){
+  var array = session;
+  alert(session);
+
+
+  array.map(function(currentValue, index){
+    var id = "nrUnits" + currentValue;
+
+    if(map.has(id)){
+      var temp = map.get(id) + 1;
+      map.set(id, temp);
+      // map[id] = map[id] + 1;
+    } else{
+      map.set(id,1);
+      // map[id] = 1;
+    }
+  });
+}
+
+function load(){
+  if(typeof(Storage) !== "undefined"){
+    for (var i = 1; i <= map.size; i++) {
+      var id = "nrUnits" + i;
+      var id_ppu = "ppu" + i;
+      var id_totalPrice = "totalPrice" + i;
+
+
+      var totalPrice = document.getElementById(id_ppu).innerHTML * map.get(id);
+
+      localStorage.setItem(id, map.get(id));
+      localStorage.setItem(id_totalPrice, totalPrice);
+
+      // alert(id + " : " + map.get(id));
+      document.getElementById(id).innerHTML = localStorage.getItem(id);
+      document.getElementById(id_totalPrice).innerHTML = localStorage.getItem(id_totalPrice) + "£";
+      // localStorage.getItem(id);
+      // $("#"+id).text(map.get(id));
+    }
+  } else {
+    alert("Sorry, your browser does not support Web Storage...");
+  }
+}
+// function updateNumberOfOrders2(){
+//   var ses = session.sort(); //getting session
+//   var rObj = {}; // initializing
+//   var uniqueSes = [];
+//
+//   ses.map(function(currentValue, index){
+//     if(rObj.hasOwnProperty(currentValue)){
+//       rObj[currentValue] = rObj[currentValue] + 1;
+//     } else {
+//       rObj[currentValue] = 1;
+//     }
+//   });
+//   alert("aa" + rObj);
+//   for(var keys in rObj){
+//     var units = "nrUnits" + rObj[keys];
+//     var totalprice = "totalPrice" + rObj[keys];
+//     alert(units + " " + totalPrice);
+//   }
+//
+// }
+function updateNumberOfOrders(){
+  var ses = session.sort(); //getting session
+  var rObj = {} // initializing
+  var uniqueSes = [];
+
+  // Create an array of non duplicates
+  $.each(ses,function(i,el){
+    if($.inArray(el,uniqueSes) === -1) uniqueSes.push(el);
+  })
+
+  // Update number of orders
+  for (var i = 0; i < session.length; i++) {
+    var id = "nrUnits" + ses[i];
+    var temp = +document.getElementById(id).innerHTML
+    temp +=1;
+    $("#"+id).text(temp);
+  }
+};
+
+//
+// window.onload = function WindowLoad(){
+//   var QS = ParseQueryString();
+//   var reloaded = QS["reloaded"];
+//   if (reloaded === "1") {
+//     updateNumberOfOrders();
+//   }
+// };
+
+window.onbeforeunload = function(){
+  load();
+}
 
 $(document).ready(function(){
-  // alert('hello');
-  // menu = $('.menus_class').data('menus');
-  // alert("Name: " + menu);
-
   $('[data-js-hide-link]').click(function(event){
     alert("The pizza has been added to you basket!");
     event.preventDefault();
-  })
-})
+  });
+
+  updateNumberOfOrders3();
+  load();
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,44 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_session
+  # before_action :initialize_value
   before_action :load_cart
 
   def add_to_cart
     id = params[:id].to_i
     session[:cart] << id
     redirect_to root_path
+    puts session[:cart]
   end
+
+
+  # def add_to_cart
+  #   id = params[:id].to_i
+  #
+  #   if @value.has_key?(id)
+  #     @value[id] += 1
+  #   else
+  #     @value[id] = 1
+  #   end
+  #   puts @value
+  #   session[:cart].clear
+  #   session[:cart] << @value
+  #   redirect_to root_path
+  # end
+
+  # def add_to_cart
+  #   value ||= Hash.new
+  #
+  #   id = params[:id].to_i
+  #   if($value.has_key?(id))
+  #     temp = $value.fetch(id) + 1
+  #     $value[id] = temp
+  #   else
+  #     $value[id] = 1
+  #   end
+  #   session[:cart] << $value
+  #   # session[:cart] << id
+  #   redirect_to root_path
+  # end
 
   def remove_from_cart
     id = params[:id].to_i
@@ -21,8 +53,14 @@ class ApplicationController < ActionController::Base
     session[:cart] ||= []
   end
 
+  # def initialize_value
+  #   @value ||= Hash.new
+  # end
+
   def load_cart
-    @cart = Menu.find(session[:cart])
+     @cart = Menu.find(session[:cart])
+     puts(session[:cart])
+     puts(@cart)
   end
 
 end
